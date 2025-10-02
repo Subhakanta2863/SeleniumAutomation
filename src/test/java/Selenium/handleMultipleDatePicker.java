376b1b732229b7342ec94f2f45b565c25d9457f5
SUBHAKANTA PATTANAIK <subha@DESKTOP-8C2ENBF.(none)>
2025-10-03 00:25:56 +0530

    Added new scenario for date pickers

---
+++ src/test/java/Selenium/handleMultipleDatePicker.java
@@ -0,0 +1,18 @@
+package Selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class handleMultipleDatePicker {
+
+	public static void main(String[] args) {
+		WebDriver driver =new ChromeDriver();
+        driver.navigate().to("");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.close();
+	}
+
+}
