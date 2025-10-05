d1a84424ed30c8e01318c0118817ae9e16e4f406
SUBHAKANTA PATTANAIK <subha@DESKTOP-8C2ENBF.(none)>
2025-10-05 20:01:40 +0530

    Datepicker changes

--- src/test/java/Selenium/handleMultipleDatePicker.java
+++ src/test/java/Selenium/handleMultipleDatePicker.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class handleMultipleDatePicker {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver =new ChromeDriver();
-        driver.navigate().to("");
+        driver.navigate().to("https://jqueryui.com/datepicker/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        Thread.sleep(5000);
         driver.close();
 	}
 
